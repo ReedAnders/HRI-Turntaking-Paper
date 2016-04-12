57acd44061334060a3d28cc4f0c1a7c7b2ce0fcb
ReedAnders <anderson.reed@gmail.com>
2016-04-12 17:04:08 -0600

    adding speech prompts and delay

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from naoqi import ALProxy
+# from naoqi import ALProxy
 
 # Connect to NAO
-# tts = ALProxy("ALTextToSpeech", "<IP of your robot>", 9559)
-tts = ALProxy("ALTextToSpeech", "nao.local", 9559)
+# # tts = ALProxy("ALTextToSpeech", "<IP of your robot>", 9559)
+# tts = ALProxy("ALTextToSpeech", "nao.local", 9559)
+
 
 # Run Script
-tts.say("Hello, world!")
+# tts.say("Hello, world!")
+
+from getch import getch
+from time import sleep
+
+convo_prompts = ['Hi, my name is Nao', 'This is an audio test']
+pointer = 0
+
+# Set for 500ms delay
+wait = True
+
+def callAudio(text):
+	if wait:
+		sleep(0.5)
+		
+	# tts.say(text)
+	print text
+
+while True:
+    key = ord(getch())
+    print key
+
+    # Press escape (or arrow keys) to exit
+    if key == 27:
+    	break
+
+    # Press spacebar play audio and iterate
+    elif key == 32:
+    	
+    	try:
+    		callAudio(convo_prompts[pointer])
+    		pointer += 1
+    	except IndexError:
+    		print "Audio ended, thanks."
+    		break
