7d636f25e1c68bcdc8afdb898248f324d2bf72aa
ReedAnders <anderson.reed@gmail.com>
2016-04-13 11:36:47 -0600

    adding experiment procedure

--- main.py
+++ main.py
@@ -14,12 +14,20 @@ from time import sleep
 convo_prompts = ['Hi, my name is Nao', 'This is an audio test']
 pointer = 0
 
-# Set for 500ms delay
+## Experiment Procedure
+
+# 1) Participant initiates conversation with Nao
+# 2) Experimenter presses space bar to make Nao respond
+# 		a) Experimenter should press play bar as if they themselves are responding
+# 3) If factor delay is on, set 'wait' variable to 'True'
+
 wait = True
 
 def callAudio(text):
+
+	# Set for 750ms delay (750ms + 250ms (Experimenter delay) = 1s delay)
 	if wait:
-		sleep(0.5)
+		sleep(0.75)
 
 	# tts.say(text)
 	print text
